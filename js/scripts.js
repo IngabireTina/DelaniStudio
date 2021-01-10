880e6b363660a6036663df2d4948d64bc6e18ff2
IngabireTina <ingabire067@gmail.com>
2021-01-10 21:06:09 +0200

    added some js toggle function on icons for what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,37 @@
-// $(document).ready(function(){
-//     $("img#logo1").click(function(){
-//         $("p1").toggle();
-//         $("img").toggle();
-//     });
-  
-// });
+$(document).ready(function(){
+    $("#design_img").click(function(){
+        $("#design_img").toggle();
+        $("#design_desc").toggle();
+
+    });
+    $("#design_desc").click(function(){
+        $("#design_img").toggle();
+        $("#design_desc").toggle();
+
+    });
+
+    $("#development_img").click(function(){
+        $("#development_img").toggle();
+        $("#development_desc").toggle();
+
+    });
+    $("#development_desc").click(function(){
+        $("#development_img").toggle();
+        $("#development_desc").toggle();
+
+    });
+
+    $("#product_img").click(function(){
+        $("#product_img").toggle();
+        $("#product_desc").toggle();
+
+    });
+    $("#product_desc").click(function(){
+        $("#product_img").toggle();
+        $("#product_desc").toggle();
+
+    });
+
+
+
+});
