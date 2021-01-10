5812f4da9846b7e6b2a18f61d5a5e9be9ea5458f
IngabireTina <ingabire067@gmail.com>
2021-01-10 21:55:54 +0200

    added some content about portfolio section

--- js/scripts.js
+++ js/scripts.js
@@ -32,6 +32,77 @@ $(document).ready(function(){
 
     });
 
+/////////////////
+     $("#work1").hover(function(){
+         $("#work1-heading").addClass("work-title-show")
+         $("#div#work1").addClass("work-section")
+     },
+     function(){
+        $("#work1-heading").removeClass("work-title-show")
+        $("#div#work1").removeClass("work-section")
+     });
 
+     $("#work2").hover(function(){
+        $("#work2-heading").addClass("work-title-show")
+        $("#div#work2").addClass("work-section")
+    },
+    function(){
+       $("#work2-heading").removeClass("work-title-show")
+       $("#div#work2").removeClass("work-section")
+    });
 
+    $("#work3").hover(function(){
+        $("#work3-heading").addClass("work-title-show")
+        $("#div#work3").addClass("work-section")
+    },
+    function(){
+       $("#work3-heading").removeClass("work-title-show")
+       $("#div#work3").removeClass("work-section")
+    });
+
+    $("#work4").hover(function(){
+        $("#work4-heading").addClass("work-title-show")
+        $("#div#work4").addClass("work-section")
+    },
+    function(){
+       $("#work4-heading").removeClass("work-title-show")
+       $("#div#work4").removeClass("work-section")
+    });
+
+    $("#work5").hover(function(){
+        $("#work5-heading").addClass("work-title-show")
+        $("#div#work5").addClass("work-section")
+    },
+    function(){
+       $("#work5-heading").removeClass("work-title-show")
+       $("#div#work5").removeClass("work-section")
+    });
+   
+
+    $("#work6").hover(function(){
+        $("#work6-heading").addClass("work-title-show")
+        $("#div#work6").addClass("work-section")
+    },
+    function(){
+       $("#work6-heading").removeClass("work-title-show")
+       $("#div#work6").removeClass("work-section")
+    });
+
+    $("#work7").hover(function(){
+        $("#work7-heading").addClass("work-title-show")
+        $("#div#work7").addClass("work-section")
+    },
+    function(){
+       $("#work7-heading").removeClass("work-title-show")
+       $("#div#work7").removeClass("work-section")
+    });
+
+    $("#work8").hover(function(){
+        $("#work8-heading").addClass("work-title-show")
+        $("#div#work8").addClass("work-section")
+    },
+    function(){
+       $("#work8-heading").removeClass("work-title-show")
+       $("#div#work8").removeClass("work-section")
+    });
 });
